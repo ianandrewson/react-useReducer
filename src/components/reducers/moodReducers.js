9f142c98cdf5fe194d698a5ea5df7922193bf1fc
Ian Andrewson <ianandrewson@gmail.com>
2020-02-12 15:28:36 -0800

    added variable names to actions instead of strings

--- src/components/reducers/moodReducers.js
+++ src/components/reducers/moodReducers.js
@@ -1,12 +1,13 @@
+import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from '../actions/moodActions.js';
 export default function reducer(state, action) {
   switch(action.type) {
-    case 'DRINK_COFFEE':
+    case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
-    case 'EAT_SNACK':
+    case EAT_SNACK:
       return { ...state, snacks: state.snacks + 1 };
-    case 'TAKE_NAP':
+    case TAKE_NAP:
       return { ...state, naps: state.naps + 1 };
-    case 'STUDY':
+    case STUDY:
       return { ...state, studies: state.studies + 1 };
     default:
       return state;
