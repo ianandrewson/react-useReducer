afbbc955b9db3570071c989ce56c97eb48ec897d
Ian Andrewson <ianandrewson@gmail.com>
2020-02-12 14:38:07 -0800

    moodActions implemented, tests pass

--- src/components/actions/moodActions.js
+++ src/components/actions/moodActions.js
@@ -1,15 +1,15 @@
 export const drinkCoffee = () => {
-
+  return { type: 'DRINK_COFFEE' };
 };
 
 export const eatSnack = () => {
-
+  return { type: 'EAT_SNACK' };
 };
 
 export const takeNap = () => {
-
+  return { type: 'TAKE_NAP' };
 };
 
 export const study = () => {
-
+  return { type: 'STUDY' };
 };
