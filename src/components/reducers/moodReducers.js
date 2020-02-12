34312539e23c5c70e57d8037b6eeaa3f71cfc105
Ian Andrewson <ianandrewson@gmail.com>
2020-02-12 15:23:27 -0800

    all action types implemented in reducer. Tests pass. All tests pass

--- src/components/reducers/moodReducers.js
+++ src/components/reducers/moodReducers.js
@@ -3,6 +3,12 @@ export default function reducer(state, action) {
     case 'DRINK_COFFEE':
       return { ...state, coffees: state.coffees + 1 };
     case 'EAT_SNACK':
-      return { ...state, snacks: state.snacks + 0 };
+      return { ...state, snacks: state.snacks + 1 };
+    case 'TAKE_NAP':
+      return { ...state, naps: state.naps + 1 };
+    case 'STUDY':
+      return { ...state, studies: state.studies + 1 };
+    default:
+      return state;
   }
 }
