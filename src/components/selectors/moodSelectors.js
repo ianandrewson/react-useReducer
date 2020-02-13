2315a5acea5256ad3eeaa2dac4febb43371eebb2
Ian Andrewson <ianandrewson@gmail.com>
2020-02-12 18:42:23 -0800

    all tests pass

--- src/components/selectors/moodSelectors.js
+++ src/components/selectors/moodSelectors.js
@@ -6,9 +6,9 @@ export const getFace = state => {
 
   if(isTired(state) && isHungry(state)) return '😠';
   if(isHyper(state) && isHungry(state)) return '😱';
-  if(isEducated(state)) return '😲';
   if(isTired(state)) return '😴';
   if(isHyper(state)) return '🙀';
+  if(isEducated(state)) return '😲';
   if(isHungry(state)) return '😡';
 
   return '😀';
--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,69 +1,21 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
+import reducer from '../components/reducers/moodReducers.js';
+import { getFace } from '../components/selectors/moodSelectors.js';
+import { drinkCoffee, eatSnack, takeNap, study } from '../components/actions/moodActions.js';
 
-const actions = [
-  { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees' },
-  { name: 'EAT_SNACK', text: 'Snack', stateName: 'snacks' },
-  { name: 'TAKE_NAP', text: 'Nap', stateName: 'naps' },
-  { name: 'STUDY', text: 'Study', stateName: 'studies' },
-];
+export default function Moods() {
+  const [mood, dispatch] = useReducer(reducer, { coffees: 0, snacks: 0, naps: 0, studies: 0 });
+  const face = getFace(mood);
+  //this next line feels and looks super wrong and dirty to me
+  const actions = [{ action: drinkCoffee, count: mood.coffees }, { action: eatSnack, count: mood.snacks }, { action: takeNap, count: mood.naps }, { action: study, count: mood.studies }];
 
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
-
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '😠';
-  if(isHyper(state) && isHungry(state)) return '😱';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '😲';
-  if(isHungry(state)) return '😡';
-
-  return '😀';
-};
-
-export default class Moods extends Component {
-  state = {
-    coffees: 0,
-    snacks: 0,
-    naps: 0,
-    studies: 0
-  }
-
-  handleSelection = name => {
-    switch(name) {
-      case 'DRINK_COFFEE':
-        this.setState(state => ({ coffees: state.coffees + 1 }));
-        break;
-      case 'EAT_SNACK':
-        this.setState(state => ({ snacks: state.snacks + 1 }));
-        break;
-      case 'TAKE_NAP':
-        this.setState(state => ({ naps: state.naps + 1 }));
-        break;
-      case 'STUDY':
-        this.setState(state => ({ studies: state.studies + 1 }));
-        break;
-      default:
-        console.log(`unhandled name: ${name}`);
-    }
-  }
-
-  render() {
-    const face = getFace(this.state);
-    const controlActions = actions.map(action => ({
-      ...action,
-      count: this.state[action.stateName]
-    }));
-
-    return (
-      <>
-        <Controls actions={controlActions} handleSelection={this.handleSelection}/>
-        <Face emoji={face} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Controls actions={actions} dispatch={dispatch} mood={mood}/>
+      <Face emoji={face} />
+    </>
+  );
 }
+
